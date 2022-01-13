74c5c9a8f79db9fa39381a1480789cdffb7e1f03
Toufik FERHAT <ferhattoufik@outlook.fr>
2022-01-13 16:20:41 +0100

    Update grpc tool to generate python code

--- grpc/grpc_client.py
+++ grpc/grpc_client.py
@@ -2,58 +2,48 @@
 This program is used to implement a gRPC client that sends rgb images to the server.
 """
 
-# import the grpc package
-import grpc
-# import the protobufs package
-from rgb_image_pb2 import RGB_image
+import grpc 
 
-# import needed modules
+# import the generated classes
+import rgb_image_pb2
+import rgb_image_pb2_grpc
+
+# Import libraries for data encoding and decoding
+import base64
+import io
+import zlib
 import numpy as np
 import cv2
-import time
-import os
-import sys
-import glob
-
-# Define the client class
-class Client():
-    def __init__(self):
-        # Create a channel to the server
-        self.channel = grpc.insecure_channel('localhost:50051')
-        # Create a stub (client)
-        self.stub = grpc.rpc_channel_ready_future(self.channel)
-        # Create a list to store the rgb images
-        self.rgb_images = []
-        # Create a list to store the rgb images
-        self.rgb_images_count = 0
-    
-    def send_rgb_image(self, rgb_image):
-        # Create a rgb image object
-        rgb_image_object = RGB_image()
-        # Assign the rgb image object the rgb image
-        rgb_image_object.images.append(rgb_image)
-        # Send the rgb image object to the server
-        self.stub.result().send(rgb_image_object)
-    
-    def receive_rgb_images(self):
-        # Create a rgb image object
-        rgb_image_object = RGB_image()
-        # Receive the rgb image object from the server
-        rgb_image_object = self.stub.result().recv_message()
-        # Assign the rgb image object to the rgb images list
-        self.rgb_images = rgb_image_object.images
-        # Assign the rgb images count to the rgb images count
-        self.rgb_images_count = len(self.rgb_images)
-    
-    def close_channel(self):
-        # Close the channel
-        self.channel.close()
-
-    def get_rgb_images(self):
-        return self.rgb_images
-    
-    def get_rgb_images_count(self):
-        return self.rgb_images_count
 
 
-    
+# create a channel
+channel = grpc.insecure_channel('localhost:50051')
+
+# create a stub (client)
+stub = rgb_image_pb2_grpc.Predict_labelStub(channel)
+
+# create a valid rgb image
+rgb_image = rgb_image_pb2.RGB_image()
+
+# Reade the image from the file
+image = cv2.imread("../data/AE00008_143414_00_1_1_2001.jpg")
+
+# Encode the image
+image_bytes = image.tobytes()
+
+# Compress the image
+image_bytes_compressed = zlib.compress(image_bytes)
+
+# Encode the image
+image_bytes_encoded = base64.b64encode(image_bytes_compressed)
+
+# Set the image
+rgb_image.image = image_bytes_encoded
+
+# Send the image to the server
+response = stub.Predict(rgb_image)
+
+# decode the response
+print("Received response:")
+print(response)
+
--- grpc/grpc_server.py
+++ grpc/grpc_server.py
@@ -1,69 +1,68 @@
 """
-This program is a gRPC server that receives rgb images from the client
+This program is a gRPC server that receives rgb images from the client.
 """
 
 # import the grpc package
 import grpc
-# import the protobufs package
-from rgb_image_pb2 import RGB_image
+from concurrent import futures
 
-# import needed modules
+# import the generated classes
+import rgb_image_pb2_grpc 
+import rgb_image_pb2
+import time
+
+import zlib 
+import base64
 import numpy as np
 import cv2
-import time
-import os
-import sys
-import glob
 
-# Define the server class
-class Server():
-    def __init__(self):
-        # Create a channel to the server
-        self.channel = grpc.insecure_channel('localhost:50051')
-        # Create a stub (server)
-        self.stub = grpc.rpc_channel_ready_future(self.channel)
-        # Create a list to store the rgb images
-        self.rgb_images = []
-        # Create a list to store the rgb images
-        self.rgb_images_count = 0
-        
-    def receive_rgb_images(self):
-        # Create a rgb image object
-        rgb_image_object = RGB_image()
-        # Receive the rgb image object from the server
-        rgb_image_object = self.stub.result().recv_message()
-        # Assign the rgb image object to the rgb images list
-        self.rgb_images = rgb_image_object.images
-        # Assign the rgb images count to the rgb images count
-        self.rgb_images_count = len(self.rgb_images)
-        
-    def get_rgb_images(self):
-        return self.rgb_images
-        
-    def get_rgb_images_count(self):
-        return self.rgb_images_count
-        
-    def close_channel(self):
-        # Close the channel
-        self.channel.close()
+class RgbImageServicer(rgb_image_pb2_grpc.Predict_labelServicer):
+    """
+    This class is used to implement a gRPC server that receives rgb images from the client.
+    """
+    def Predict(self, request, context):
+        """
+        Input RGB image
+        """
+        print("Received image:")
+        print(request)
 
-def main():
-    # Run the server
-    server = Server()
+        # decode the image and display it
+        image = np.frombuffer(zlib.decompress(base64.b64decode(request.image)), dtype=np.uint8).reshape((100, 100, 3))
 
-    # Create a loop to receive rgb images
+        # display the image
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+
+        # create a valid response
+        response = rgb_image_pb2.Predicted_label()
+        response.label = "false"
+        response.name = "image1"
+
+        return response
+
+
+
+
+# create a gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+# add the calculator service to the server
+rgb_image_pb2_grpc.add_Predict_labelServicer_to_server(RgbImageServicer(), server)
+
+# listen on port 50051
+print('Starting server. Listening on port 50051.')
+server.add_insecure_port('[::]:50051')
+
+# start the server
+server.start()
+
+# since server.start() will not block,
+# a sleep-loop is added to keep alive
+try:
     while True:
-        # Receive the rgb images
-        server.receive_rgb_images()
-        # Get the rgb images
-        rgb_images = server.get_rgb_images()
-        # Get the rgb images count
-        rgb_images_count = server.get_rgb_images_count()
-        # Print the rgb images count
-        print('rgb_images_count:', rgb_images_count)
-        # Print the rgb images
-        print('rgb_images:', rgb_images)
-        # Close the channel
-        server.close_channel()
-        # Wait for 1 second
-        time.sleep(1)
+        print('.')
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
+
