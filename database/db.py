43a552e228f34d46c54708e82a725b1f75d1556b
Mohamed Ali RAIS <mohamedali.rais@yahoo.fr>
2022-01-24 13:28:45 +0100

    db

--- database/db.py
+++ database/db.py
@@ -2,6 +2,8 @@ import sqlite3
 from sqlite3 import Error
 import csv
 import os
+from datetime import datetime
+
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
@@ -12,14 +14,14 @@ def create_connection(db_file):
     except Error as e:
         print(e)
 
-def insert_card(Name,DIE,classification_result,Date):
+def insert_card(Name,DIE,classification_result,Confidence,Bounding_box,Date):
     path = os.getcwd()
     path = path.replace(os.sep, '/')
     path = path + '/test.db'
     con = create_connection(path)
     cur = con.cursor()
-    cur.execute("create table if not exists Carte (Name,DIE,classification_result,Date)")
-    cur.execute("insert into Carte values (?,?,?,?)", (Name,DIE,classification_result,Date))
+    cur.execute("create table if not exists Carte (Name,DIE,classification_result,Confidence,Bounding_box,Date)")
+    cur.execute("insert into Carte values (?,?,?,?,?,?)", (Name,DIE,classification_result,Confidence,Bounding_box,Date))
     con.commit()
     con.close()
 
@@ -41,12 +43,14 @@ def save_report():
     res = generate_report()
     fp = open("report.csv", "w", newline='')
     myFile = csv.writer(fp, delimiter = ',')
-    myFile.writerow(['Card_Name','DIE','classification_result','Date'])
+    myFile.writerow(['Card_Name','DIE','classification_result','Confidence','Bounding_box','Date'])
     myFile.writerows(res)
     fp.close()
 
 if __name__ == '__main__':
-    insert_card("test_card_1",4,"Defected","2022-01-21")
+    todays_date = datetime.today().strftime('%Y-%m-%d')
+    insert_card("test_card_1",4,"Defected",99,"0,100,21,57",todays_date)
+    insert_card("test_card_2",3,"NOT Defected",97,"0,0,0,0",todays_date)
     r = generate_report()
     print(r)
     save_report()
